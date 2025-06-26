402132dfc04122f188bf72ead15b769f72ad0b56
Fatush <tushtushahmed@gmail.com>
2025-06-25 17:15:04 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>KeNHA Connect</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Welcome to KeNHA Connect</h1>
+        <p>Your trusted communication platform</p>
+        <a href="login.php"><button>Login</button></a>
+        <a href="register.php"><button>Register</button></a>
+    </div>
+</body>
+</html>
